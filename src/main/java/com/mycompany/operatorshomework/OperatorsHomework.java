9a3ee691edfa6385f8e33c27418952ae2be76e57
sudekirici <sudeekiricii@gmail.com>
2023-09-14 16:10:22 +0300

    Homework using operators successfully completed.

---
+++ src/main/java/com/mycompany/operatorshomework/OperatorsHomework.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operatorshomework;
+
+/**
+ *
+ * @author S
+ */
+public class OperatorsHomework {
+
+    public static void main(String[] args) {
+        int firstnumber = 45;
+        int secondnumber = 85;
+        
+        int totals = firstnumber + secondnumber;
+        int products = firstnumber * secondnumber;
+        
+        System.out.println("Totals: " + totals);
+        
+        if (3000 < products && products < 4000) {
+            System.out.println("Products: " + products);
+        }
+    }
+}
